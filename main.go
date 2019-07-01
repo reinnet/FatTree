15f5d6666453c7339e5db75e278f95e12beede12
Parham Alvani <parham.alvani@gmail.com>
2019-07-02 03:36:16 +0430

    feat: Create Core Switches

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
+// Node in physical server or node in fat tree topology
+type Node struct {
+	ID              string   `yaml:"id"`
+	Cores           int      `yaml:"cores"`
+	RAM             int      `yaml:"ram"`
+	VNFSupport      bool     `yaml:"vnfSupport"`
+	NotManagerNodes []string `yaml:"notManagerNodes"`
+}
+
 func main() {
+	var nodes []Node
+
 	var k int
 
 	fmt.Scanf("%d", &k)
@@ -25,4 +38,20 @@ func main() {
 	fmt.Printf("Servers: %d\n", servers)
 
 	fmt.Printf("Nodes: %d\n", cores+pods*(servers+edges+aggregations))
+
+	for i := 0; i < cores; i++ {
+		nodes = append(nodes, Node{
+			ID:              fmt.Sprintf("switch-%d", i),
+			Cores:           0,
+			RAM:             0,
+			VNFSupport:      false,
+			NotManagerNodes: []string{},
+		})
+	}
+
+	b, err := yaml.Marshal(nodes)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(b))
 }
