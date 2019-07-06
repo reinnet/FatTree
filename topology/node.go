a8457ab642026bd19743a5408b1e131301ce55f5
Parham Alvani <parham.alvani@gmail.com>
2019-07-06 23:25:37 +0430

    fix: Correct Alignment Issue in Node Structure

--- topology/node.go
+++ topology/node.go
@@ -2,11 +2,11 @@ package topology
 
 // Node is a physical server in fat tree topology
 type Node struct {
-	ID              string   `yaml:"id"`
 	Cores           int      `yaml:"cores"`
 	RAM             int      `yaml:"ram"`
 	VNFSupport      bool     `yaml:"vnfSupport"`
-	NotManagerNodes []string `yaml:"notManagerNodes"`
 	Egress          bool     `yaml:"egress"`
 	Ingress         bool     `yaml:"ingress"`
+	ID              string   `yaml:"id"`
+	NotManagerNodes []string `yaml:"notManagerNodes"`
 }
