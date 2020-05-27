d689de60d05c6351be2ebf83a0dd15c66d1f7980
Parham Alvani <parham.alvani@gmail.com>
2020-05-27 10:40:34 +0430

    feat: Remove Topology Package

--- topology/link.go
+++
@@ -1,8 +0,0 @@
-package topology
-
-// Link is a physical link in fat tree topology
-type Link struct {
-	Source      string `yaml:"source"`
-	Destination string `yaml:"destination"`
-	Bandwidth   int    `yaml:"bandwidth"`
-}
