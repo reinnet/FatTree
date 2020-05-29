36ac756c9f560fef57cc8afbb8c771f726050866
Parham Alvani <parham.alvani@gmail.com>
2020-05-29 09:34:23 +0430

    fix: Correct Lint Issues

--- fattree/namer/server.go
+++ fattree/namer/server.go
@@ -2,7 +2,7 @@ package namer
 
 import "fmt"
 
-// Server creates a server identification
+// Server creates a server identification.
 func Server(pod int, id int) string {
 	return fmt.Sprintf("server-%d-%d", pod, id)
 }
--- fattree/namer/switch.go
+++ fattree/namer/switch.go
@@ -2,17 +2,17 @@ package namer
 
 import "fmt"
 
-// EdgeSwitch creates the edge switch identification
+// EdgeSwitch creates the edge switch identification.
 func EdgeSwitch(pod int, id int) string {
 	return fmt.Sprintf("edge-switch-%d-%d", pod, id)
 }
 
-// AggrSwitch creates the aggregation switch identification
+// AggrSwitch creates the aggregation switch identification.
 func AggrSwitch(pod int, id int) string {
 	return fmt.Sprintf("aggr-switch-%d-%d", pod, id)
 }
 
-// CoreSwitch creates the core switch identification
+// CoreSwitch creates the core switch identification.
 func CoreSwitch(id int) string {
 	return fmt.Sprintf("core-switch-%d", id)
 }
